0c7060d77e5a762846019c2e2727587fd9212e5b
Alex Bethel <abethel@cs.nmt.edu>
2023-11-03 21:22:12 -0600

    Add configuration.nix

---
+++ configuration.nix
@@ -0,0 +1,22 @@
+# This file is for ros4nix.
+
+{ config, pkgs, ... }:
+{
+  # ---- Base NixOS configuration ----
+  imports = [
+    ./ros
+  ];
+
+  system.stateVersion = "23.05";
+  boot.isContainer = true;
+
+  # Change this to the architecture of your system.
+  nixpkgs.hostPlatform = "aarch64-linux";
+
+  # ---- Begin ROS configuration ----
+
+  services.ros.enable = true;
+  programs.ros.myIP = "192.168.0.235";
+  services.ros.testPublisher.enable = true;
+  services.ros.elevationMapping.build = true;
+}
