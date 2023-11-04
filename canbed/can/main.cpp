8fe3fd581f183d761ffc0cfaa55a160d13cddc2f
Alex Bethel <abethel@cs.nmt.edu>
2023-11-04 02:15:40 -0600

    Get CAN working

--- canbed/can/main.cpp
+++ canbed/can/main.cpp
@@ -13,11 +13,12 @@ enum class MotorCommand {
 };
 
 class CANCommand {
+public:
   unsigned int id;
   unsigned char buf[8];
 
-public:
-  CANCommand(unsigned int id, const unsigned char *other_buf, int buf_len) {
+  CANCommand(unsigned int id, const unsigned char *other_buf, int buf_len)
+      : id(id) {
     for (int i = 0; i < sizeof(buf); i++)
       if (i < buf_len)
         buf[i] = other_buf[i];
@@ -54,36 +55,54 @@ public:
   void dispatch(const CANCommand &command) {
     OutPin *fwd, *rev;
     if (command.is_left_motor()) {
+      Serial.println("Left");
       fwd = &left_fwd;
       rev = &left_rev;
     } else if (command.is_right_motor()) {
+      Serial.println("Right");
       fwd = &right_fwd;
       rev = &right_rev;
     } else {
       // not a wheel command, do nothing.
+      Serial.print("Not a command because ");
+      Serial.print(command.id);
+      Serial.print(" is not ");
+      Serial.print(0x100);
+      Serial.print(" or ");
+      Serial.println(0x101);
       return;
     }
 
+    double max = 0.6;
+    // Correct for discrepancy with right motor running twice as fast
+    // as left motor.
+    if (command.is_right_motor())
+      max /= 1.0;
+
     switch (command.command()) {
     case MotorCommand::Stop:
-      *fwd = 0;
-      *rev = 0;
+      Serial.println("Stop");
+      fwd->write_pwm(0);
+      rev->write_pwm(0);
       break;
     case MotorCommand::Forward:
-      *fwd = 1;
-      *rev = 0;
+      Serial.println("Forward");
+      fwd->write_pwm(max);
+      rev->write_pwm(0);
       break;
     case MotorCommand::Reverse:
-      *fwd = 0;
-      *rev = 1;
+      Serial.println("Reverse");
+      fwd->write_pwm(0);
+      rev->write_pwm(max);
       break;
     }
   }
 };
 
-int main() {
+MCP_CAN CAN(17); // Set CS pin
+
+void setup() {
   Serial.begin(115200);
-  MCP_CAN CAN(17); // Set CS pin
 
   while (CAN_OK != CAN.begin(CAN_500KBPS)) // init can bus : baudrate = 500k
   {
@@ -91,30 +110,33 @@ int main() {
     delay(100);
   }
   Serial.println("CAN BUS OK!");
+}
 
-  Wheels wheels(4, 5, 6, 8);
+void loop() {
+  // Wheels wheels(8, 6, 4, 5);
+  Wheels wheels(10, 6, 9, 5);
 
-  while (true) {
-    unsigned char len;
-    unsigned char buf[8];
+  unsigned char len;
+  unsigned char buf[8];
 
-    if (CAN.checkReceive() == CAN_MSGAVAIL) {
-      CAN.readMsgBuf(&len, buf);
-      unsigned long canId = CAN.getCanId();
+  // Serial.println("looping");
 
-      Serial.println("-----------------------------");
-      Serial.print("Get data from ID: ");
-      Serial.println(canId, HEX);
+  if (CAN.checkReceive() == CAN_MSGAVAIL) {
+    CAN.readMsgBuf(&len, buf);
+    unsigned long canId = CAN.getCanId();
 
-      // print the data
-      for (int i = 0; i < len; i++) {
-        Serial.print(buf[i], HEX);
-        Serial.print("\t");
-      }
-      Serial.println();
+    Serial.println("-----------------------------");
+    Serial.print("Get data from ID: ");
+    Serial.println(canId, HEX);
 
-      CANCommand cmd(canId, buf, len);
-      wheels.dispatch(cmd);
+    // print the data
+    for (int i = 0; i < len; i++) {
+      Serial.print(buf[i], HEX);
+      Serial.print("\t");
     }
+    Serial.println();
+
+    CANCommand cmd(canId, buf, len);
+    wheels.dispatch(cmd);
   }
 }
