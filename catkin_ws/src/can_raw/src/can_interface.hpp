3b15a4d682f9f8706179c695e5ad26623e3caa74
Alex Bethel <abethel@cs.nmt.edu>
2023-11-10 14:22:08 -0700

    Add raw CAN node

---
+++ catkin_ws/src/can_raw/src/can_interface.hpp
@@ -0,0 +1,36 @@
+#ifndef CAN_INTERFACE_H
+#define CAN_INTERFACE_H
+
+#include <linux/can.h>
+
+// Basic file descriptor wrapper. Not really intended for use outside
+// SocketCAN.
+#include <string>
+class fd {
+  int id;
+
+public:
+  fd();
+  fd(int n);
+  fd(fd &&other);
+  ~fd();
+
+  void operator=(fd &&other);
+  int operator*();
+
+  int into_raw();
+};
+
+// CAN socket.
+class SocketCAN {
+  fd sock;
+
+public:
+  SocketCAN();
+  SocketCAN(const std::string &interface);
+
+  void transmit(const struct can_frame &cf);
+  void transmit(int can_id, const uint8_t data[8]);
+};
+
+#endif /* CAN_INTERFACE_H */
---
+++ catkin_ws/src/can_raw/src/can_raw_node.cpp
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+#include <can_raw/CanFrame.h>
+#include <ros/ros.h>
+
+#include "can_interface.hpp"
+
+class Node {
+  ros::NodeHandle nh;
+  ros::Subscriber subscriber;
+  SocketCAN socket;
+
+public:
+  Node() {
+    subscriber = nh.subscribe("/canbus", 16, &Node::handle_message, this);
+    socket = SocketCAN("can0");
+  }
+
+  void handle_message(const can_raw::CanFrame &frame) {
+    socket.transmit((int)frame.id, (const uint8_t *)frame.data.data());
+  }
+};
+
+int main(int argc, char **argv) {
+  ros::init(argc, argv, "can_raw_node");
+
+  printf("Hello World!\n");
+}
