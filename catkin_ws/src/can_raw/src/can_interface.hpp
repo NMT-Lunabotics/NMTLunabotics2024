8cf0ad70a7a6bb45a4f8e3503453b6b1a20c7629
Alex Bethel <abethel@cs.nmt.edu>
2024-01-27 16:58:48 -0700

    Rewrite can_interface

--- catkin_ws/src/can_raw/src/can_interface.hpp
+++ catkin_ws/src/can_raw/src/can_interface.hpp
@@ -1,6 +1,15 @@
 #ifndef CAN_INTERFACE_H
 #define CAN_INTERFACE_H
 
+#include <cstring>
+#include <iostream>
+#include <linux/can.h>
+#include <net/if.h>
+#include <string>
+#include <sys/ioctl.h>
+#include <sys/socket.h>
+#include <unistd.h>
+
 #include <linux/can.h>
 
 // Basic file descriptor wrapper. Not really intended for use outside
@@ -10,15 +19,28 @@ class fd {
   int id;
 
 public:
-  fd();
-  fd(int n);
-  fd(fd &&other);
-  ~fd();
+  fd() : id(-1) {}
+  fd(int n) : id(n) {}
+  fd(fd &&other) {
+    id = other.id;
+    other.id = -1;
+  }
+  ~fd() {
+    if (id != -1)
+      close(id);
+  }
 
-  void operator=(fd &&other);
-  int operator*();
+  void operator=(fd &&other) {
+    id = other.id;
+    other.id = -1;
+  }
+  int operator*() { return id; }
 
-  int into_raw();
+  int into_raw() {
+    int id_ = id;
+    id = -1;
+    return id_;
+  }
 };
 
 // CAN socket.
@@ -26,11 +48,51 @@ class SocketCAN {
   fd sock;
 
 public:
-  SocketCAN();
-  SocketCAN(const std::string &interface);
+  SocketCAN() {}
+  SocketCAN(const std::string &interface) {
+    sock = socket(PF_CAN, SOCK_RAW, CAN_RAW);
+    if (*sock == -1)
+      throw std::string("Error creating socket");
 
-  void transmit(const struct can_frame &cf);
-  void transmit(int can_id, const uint8_t data[8]);
+    // Select CAN interface.
+    ifreq req;
+    strncpy(req.ifr_ifrn.ifrn_name, interface.data(),
+            sizeof(req.ifr_ifrn.ifrn_name));
+    if (ioctl(*sock, SIOCGIFINDEX, &req) == -1)
+      throw std::string("Unable to select CAN interface");
+
+    // Bind the socket to the network interface.
+    sockaddr_can addr;
+    addr.can_family = AF_CAN;
+    addr.can_ifindex = req.ifr_ifru.ifru_ivalue;
+    if (bind(*sock, reinterpret_cast<sockaddr *>(&addr), sizeof(addr)) == -1)
+      throw std::string("Failed to bind socket to network interface");
+
+    std::cout << "Successfully bound socket to interface.\n";
+  }
+
+  void transmit(const struct can_frame &cf) {
+    std::cout << "Writing CAN frame: " //
+              << "[ " << cf.data[0]    //
+              << ", " << cf.data[1]    //
+              << ", " << cf.data[2]    //
+              << ", " << cf.data[3]    //
+              << ", " << cf.data[4]    //
+              << ", " << cf.data[5]    //
+              << ", " << cf.data[6]    //
+              << ", " << cf.data[7]    //
+              << " ]\n";
+    if (write(*sock, &cf, sizeof(cf)) != sizeof(cf))
+      throw std::string("Error transmitting CAN frame");
+  }
+
+  void transmit(int can_id, const uint8_t data[8]) {
+    struct can_frame frame;
+    frame.can_id = can_id;
+    frame.can_dlc = 8 * sizeof(data[0]);
+    memcpy(frame.data, data, frame.can_dlc);
+    transmit(frame);
+  }
 };
 
 #endif /* CAN_INTERFACE_H */
