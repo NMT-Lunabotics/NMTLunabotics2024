2a91a0a4af786eff3930eadd8dfe2ae0dbba9c29
jhunter533 <jhunter533@gmail.com>
2024-03-05 20:53:11 -0700

    add pid

--- canbed/linear_actuators/linear_actuators_pid.ino
+++ canbed/linear_actuators/linear_actuators_pid.ino
@@ -1,62 +1,4 @@
-#include <Arduino.h>
-
-// Define pin numbers
-const int spd = 6;
-const int dir = 7;
-const int potPin = A0;
-
-int tgt = 100;      // in mm
-
-int pwm = 200;      // speed, in pwm TODO change this to mm/s
-int stroke = 250;   // stroke length, in mm
-int potMin = 34;    // Calibrated, pot val at min stroke
-int potMax = 945;   // Calibrated, pot val at max stroke
-float weight = 0.2; // How much old readings influence new ones
-float kp=.16;
-float ki=.001;
-float kd=.01;
-
-float integral=0;
-float prevError=0;
-
-float integral=0;
-int prevError=0;
-void setup() {
-  Serial.begin(9600);
-  pinMode(spd, OUTPUT);
-  pinMode(dir, OUTPUT);
-  pinMode(potPin, INPUT);
-
-  int lastValue = analogRead(potPin);
-  while (true) {
-    // Handle new readings
-    int newVal = analogRead(potPin);
-
-    if (abs(newVal - lastValue) > 20) {
-      Serial.println("Ignoring jump");
-      lastValue = newVal;
-      delay(50);
-      continue;
-    }
-
-    lastValue = newVal;
-
-    int pos = map(newVal, potMin, potMax, 0, stroke);
-    Serial.print(newVal);
-    Serial.print("\t:\t");
-    Serial.println(pos);
-
-    delay(50);
-
-    if (pos > tgt) {
-	      
-    } 
-  }
-}
-
-void loop() {}
-~                                                                                                        
-~   #include <Arduino.h>
+ #include <Arduino.h>
 
 // Define pin numbers
 const int spd = 6;
@@ -74,7 +16,10 @@ float weight = 0.2; // How much old readings influence new ones
 float kP=.2;
 float kI=.001;
 float kD=.01;
-
+float derivative;
+float error;
+float prevError=0;
+float integral=0;
 void setup() {
   Serial.begin(9600);
   pinMode(spd, OUTPUT);
@@ -101,10 +46,13 @@ void setup() {
     Serial.println(pos);
 
     delay(50);
-    int error=tgt-pos;
+    
+    error=tgt-pos;
+    derivative=error-prevError;
+    prevError=error;
     integral+=error;
     int output=kP*error+kI*integral+kD*derivative;
-    output=constrain(output,-256,256);
+    output=constrain(output,0,250);
     if (output > 0) {
       digitalWrite(dir, LOW);
       analogWrite(spd, abs(output));
@@ -114,7 +62,7 @@ void setup() {
     } else {
       analogWrite(spd, 0);
     }
-      prevError=error;
+     
   }
 }
 
