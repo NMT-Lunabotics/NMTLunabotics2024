ad171a90a25035c9e6a93ea2f5fdaf584db7035b
Alex Bethel <abethel@cs.nmt.edu>
2024-04-18 21:47:31 -0600

    Let's try that once more

--- catkin_ws/src/heartbeat/src/heartbeat_client_node.cpp
+++ catkin_ws/src/heartbeat/src/heartbeat_client_node.cpp
@@ -1,6 +1,6 @@
 #include "ros/ros.h"
-#include "std_msgs/Empty.h"
 #include "std_msgs/Bool.h"
+#include "std_msgs/Empty.h"
 
 class HeartbeatClient
 {
@@ -8,6 +8,7 @@ private:
     ros::NodeHandle nh;
     ros::Subscriber sub;
     ros::Publisher pub;
+    ros::Publisher led_pub;
     ros::Timer timer;
     bool heartbeat_received;
 
@@ -16,22 +17,33 @@ public:
     {
         sub = nh.subscribe("/heartbeat", 1000, &HeartbeatClient::heartbeat_callback, this);
         pub = nh.advertise<std_msgs::Bool>("/stop", 1000);
+        led_pub = nh.advertise<std_msgs::Bool>("/leds/motion", 16);
         timer = nh.createTimer(ros::Duration(1.0), &HeartbeatClient::check_heartbeat, this);
         heartbeat_received = false;
     }
 
-    void heartbeat_callback(const std_msgs::Empty::ConstPtr& msg)
+    void heartbeat_callback(const std_msgs::Empty::ConstPtr &msg)
     {
         heartbeat_received = true;
     }
 
-    void check_heartbeat(const ros::TimerEvent&)
+    void check_heartbeat(const ros::TimerEvent &)
     {
         if (!heartbeat_received)
         {
             std_msgs::Bool stop_msg;
             stop_msg.data = true;
             pub.publish(stop_msg);
+
+            std_msgs::Bool led_msg;
+            led_msg.data = false;
+            led_pub.publish(led_msg);
+        }
+        else
+        {
+            std_msgs::Bool led_msg;
+            led_msg.data = true;
+            led_pub.publish(led_msg);
         }
         heartbeat_received = false;
     }
@@ -46,4 +58,3 @@ int main(int argc, char **argv)
 
     return 0;
 }
-
