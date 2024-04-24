fc66820f00be703fd1f456a53bddcc9c29d3bac0
Committed from Jetson (smelly) <lunabotics@lunabotics.nowhere>
2024-04-23 23:20:06 -0600

    Jetson work

--- catkin_ws/src/mapping/src/cvt_occupancy.cpp
+++ catkin_ws/src/mapping/src/cvt_occupancy.cpp
@@ -18,7 +18,6 @@ nav_msgs::OccupancyGrid convertToOccupancyGrid(const grid_map::GridMap& map, con
     grid_map::GridMapRosConverter::toOccupancyGrid(map, layer, dataMax, dataMin, occupancy_grid);
     // TODO make this a parameter
     for (auto& value : occupancy_grid.data) {
-	std::cout << traversability_cutoff << "\n";
         if (value < traversability_cutoff) {
             value = 0;
         } else {
