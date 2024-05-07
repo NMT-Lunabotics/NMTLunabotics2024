72579693bca911f0616338d6f9f00358556c367b
Alex Bethel <abethel@cs.nmt.edu>
2024-05-06 22:34:20 -0600

    Make tf republish on a timer

--- catkin_ws/src/mapping/src/april_tag_loc.cpp
+++ catkin_ws/src/mapping/src/april_tag_loc.cpp
@@ -4,49 +4,32 @@
 #include <tf/transform_broadcaster.h>
 #include <tf/transform_listener.h>
 
-// Get the transforms that will be added together (i.e. d435 and t265)
-bool getTransform(const std::string &from_frame, const std::string &to_frame,
-                  tf::StampedTransform &transform, const tf::TransformListener &listener)
-{
-    // Wait until the transform is available
-    try
-    {
-        listener.waitForTransform(from_frame, to_frame, ros::Time(0), ros::Duration(1.0));
-        listener.lookupTransform(from_frame, to_frame, ros::Time(0), transform);
-        return true;
-    }
-    catch (tf::TransformException &ex)
-    {
-        ROS_ERROR("%s", ex.what());
-        return false;
-    }
-}
+static bool have_transform = false;
+static tf::Transform transform;
 
 // Broadcast the new apriltag to t265 odom frame
-void broadTF(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg,
-             const tf::TransformListener &listener, tf::TransformBroadcaster &broadcaster)
+void calculate_tf(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg,
+                  const tf::TransformListener &listener, tf::TransformBroadcaster &broadcaster)
 {
-    tf::StampedTransform d435_to_t265;
     // If the detection is found
     if (!msg->detections.empty())
     {
-        // Get the tranform between the d435 and t265
-        if (getTransform("d435_link", "t265_link", d435_to_t265, listener))
-        {
+        // Set first april tag detected pose
+        const auto &detection = msg->detections[0];
 
-            // Set first april tag detected pose
-            const auto &detection = msg->detections[0];
-
-            // Transform the detected apriltag pose into a tf and add it to the cameras-tf
-            tf::Pose tag_transform;
-            tf::poseMsgToTF(detection.pose.pose.pose, tag_transform);
-            tf::Transform adjusted_transform = d435_to_t265 * tag_transform;
-
-            // Broadcast new transform from "map" to "t265_odom_frame"
-            broadcaster.sendTransform(tf::StampedTransform(adjusted_transform, ros::Time::now(),
-                                                           "map", "t265_odom_frame"));
-        }
+        // Transform the detected apriltag pose into a tf and add it to the cameras-tf
+        tf::Pose tag_transform;
+        tf::poseMsgToTF(detection.pose.pose.pose, tag_transform);
+        have_transform = true;
+        transform = tag_transform;
     }
+}
+
+void broadcast_tf(tf::TransformBroadcaster &broadcaster)
+{
+    if (have_transform)
+        broadcaster.sendTransform(
+            tf::StampedTransform(transform, ros::Time::now(), "map", "d435_link"));
 }
 
 struct callback_data
@@ -56,7 +39,12 @@ struct callback_data
 
     void aprilTagCallback(const apriltag_ros::AprilTagDetectionArray::ConstPtr &a)
     {
-        broadTF(a, listener, broadcaster);
+        calculate_tf(a, listener, broadcaster);
+    }
+
+    void timerCallback(const ros::TimerEvent &ev)
+    {
+        broadcast_tf(broadcaster);
     }
 };
 
@@ -74,6 +62,9 @@ int main(int argc, char **argv)
     // Subscribe to AprilTag detections
     ros::Subscriber sub =
         node.subscribe("tag_detections", 1, &callback_data::aprilTagCallback, &data);
+
+    // Set a timer to republish transform.
+    ros::Timer timer = node.createTimer(0.1, &callback_data::timerCallback, &data);
 
     ros::spin();
     return 0;
