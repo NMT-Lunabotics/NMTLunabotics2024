4e7b76e1aee19cf18827b205e046cc6cbbe70e42
Alex Bethel <abethel@cs.nmt.edu>
2024-05-07 17:16:06 -0600

    Add the basic digging autonomy service

---
+++ catkin_ws/src/digging_autonomy/src/digging_autonomy_node.cpp
@@ -0,0 +1,16 @@
+#include <digging_autonomy/execute_digging_autonomy.h>
+#include <ros/ros.h>
+
+bool handle_service(digging_autonomy::execute_digging_autonomyRequest &req,
+                    digging_autonomy::execute_digging_autonomyResponse &res)
+{
+  return true;
+}
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "digging_autonomy");
+
+    ros::NodeHandle nh;
+    ros::ServiceServer service = nh.advertiseService("excavation", handle_service);
+}
