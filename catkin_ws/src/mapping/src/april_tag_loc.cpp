b4393f9b5ea5b6871afaba606d0da6bbdfdc35fa
Alex Bethel <abethel@cs.nmt.edu>
2024-05-06 23:03:52 -0600

    Transforms from map to tag

--- catkin_ws/src/mapping/src/april_tag_loc.cpp
+++ catkin_ws/src/mapping/src/april_tag_loc.cpp
@@ -5,7 +5,25 @@
 #include <tf/transform_listener.h>
 
 static bool have_transform = false;
-static tf::Transform transform;
+static tf::Transform map_to_tag;
+
+// Get the transforms that will be added together (i.e. d435 and t265)
+bool getTransform(const std::string &from_frame, const std::string &to_frame,
+                  tf::StampedTransform &transform, const tf::TransformListener &listener)
+{
+    // Wait until the transform is available
+    try
+    {
+        listener.waitForTransform(from_frame, to_frame, ros::Time(0), ros::Duration(1.0));
+        listener.lookupTransform(from_frame, to_frame, ros::Time(0), transform);
+        return true;
+    }
+    catch (tf::TransformException &ex)
+    {
+        ROS_ERROR("%s", ex.what());
+        return false;
+    }
+}
 
 // Broadcast the new apriltag to t265 odom frame
 void calculate_tf(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg,
@@ -14,14 +32,18 @@ void calculate_tf(const apriltag_ros::AprilTagDetectionArray::ConstPtr &msg,
     // If the detection is found
     if (!msg->detections.empty())
     {
-        // Set first april tag detected pose
-        const auto &detection = msg->detections[0];
+        tf::StampedTransform map_to_d435;
+        if (getTransform("map", "d435", map_to_d435, listener))
+        {
+            // Set first april tag detected pose
+            const auto &detection = msg->detections[0];
 
-        // Transform the detected apriltag pose into a tf and add it to the cameras-tf
-        tf::Pose tag_transform;
-        tf::poseMsgToTF(detection.pose.pose.pose, tag_transform);
-        have_transform = true;
-        transform = tag_transform;
+            // Transform the detected apriltag pose into a tf and add it to the cameras-tf
+            tf::Pose d435_to_tag;
+            tf::poseMsgToTF(detection.pose.pose.pose, d435_to_tag);
+            have_transform = true;
+            map_to_tag = map_to_d435 * d435_to_tag;
+        }
     }
 }
 
@@ -31,8 +53,7 @@ void broadcast_tf(tf::TransformBroadcaster &broadcaster)
     {
         std::cout << "Broadcasting transform\n";
 
-        broadcaster.sendTransform(
-            tf::StampedTransform(transform, ros::Time::now(), "d435_link", "map"));
+        broadcaster.sendTransform(tf::StampedTransform(map_to_tag, ros::Time::now(), "map", "tag"));
     }
 }
 
