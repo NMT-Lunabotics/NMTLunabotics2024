49bd7d7f2cff035be9b4a3fa9ec7bc4e8a4ac631
Niall Devlin <niallcdevlin@gmail.com>
2024-05-14 14:41:22 -0400

    no thoughts map empty

--- catkin_ws/src/mapping/src/create_occupancy.cpp
+++ catkin_ws/src/mapping/src/create_occupancy.cpp
@@ -5,7 +5,7 @@ int main(int argc, char **argv)
 {
     ros::init(argc, argv, "create_occupancy");
     ros::NodeHandle nh;
-    ros::Publisher map_pub = nh.advertise<nav_msgs::OccupancyGrid>("map", 1);
+    ros::Publisher map_pub = nh.advertise<nav_msgs::OccupancyGrid>("/map", 1);
     ros::Rate loop_rate(10); // Publishing at 10 Hz
 
     // Parameters for grid
@@ -20,8 +20,8 @@ int main(int argc, char **argv)
     grid.info.resolution = resolution;
     grid.info.width = static_cast<unsigned int>(width / resolution);
     grid.info.height = static_cast<unsigned int>(height / resolution);
-    grid.info.origin.position.x = width / 2;
-    grid.info.origin.position.y = height / 2;
+    grid.info.origin.position.x = -width / 2;
+    grid.info.origin.position.y = -height / 2;
     grid.info.origin.position.z = 0.0;
     grid.info.origin.orientation.w = 1.0;
     grid.data.resize(grid.info.width * grid.info.height, 0); // 0 indicates fully traversable
