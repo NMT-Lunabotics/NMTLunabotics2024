462cc3057280b2aa67c327203c187071954b882e
Niall Devlin <niallcdevlin@gmail.com>
2024-05-16 09:19:46 -0400

    added markers

--- catkin_ws/src/hud/src/hud_node.cpp
+++ catkin_ws/src/hud/src/hud_node.cpp
@@ -9,19 +9,28 @@
 
 class HudNode {
 public:
-  HudNode() : it_(nh_) {
+  HudNode() : it_(private_nh_) {
     // Initialize subscribers and publishers
-    // Get the topic names from parameters
-    std::string image_input_topic, image_output_topic;
-    nh_.param<std::string>("image_input_topic", image_input_topic, "/usb_cam/image_raw");
-    nh_.param<std::string>("image_output_topic", image_output_topic, "/camera_hud");
+    // Get the topic names from parameters in the private namespace
+    std::string image_in, image_out;
+    private_nh_.param<std::string>("image_in", image_in, "/usb_cam/image_raw");
+    private_nh_.param<std::string>("image_out", image_out, "/camera_hud");
 
-    image_sub_ = it_.subscribe(image_input_topic, 1, &HudNode::imageCb, this,
+    image_sub_ = it_.subscribe(image_in, 1, &HudNode::imageCb, this,
                                image_transport::TransportHints("compressed"));
-    image_pub_ = it_.advertise(image_output_topic, 1);
+    image_pub_ = it_.advertise(image_out, 1);
 
-    text_sub_ = nh_.subscribe<can_convert::ArmStatus>("/arm_status", 1,
-                                                      &HudNode::textCb, this);
+    text_sub_ = private_nh_.subscribe<can_convert::ArmStatus>(
+        "/arm_status", 1, &HudNode::textCb, this);
+  }
+
+  void textCb(const can_convert::ArmStatus::ConstPtr &msg) {
+    arm_angle = msg->arm_angle * 3.14 / 180;
+    bucket_angle = msg->bucket_angle * 3.14 / 180;
+    std::stringstream ss;
+    ss << "Bucket Angle: " << int(-bucket_angle)
+       << " deg, Arm Angle: " << int(arm_angle) << " deg";
+    overlay_text = ss.str();
   }
 
   void imageCb(const sensor_msgs::ImageConstPtr &msg) {
@@ -34,24 +43,39 @@ public:
       return;
     }
 
-    // Draw text and shapes on the video stream based on the robot arm status
-    // Drawing logic remains the same as before
+    int start_x = 500;
+    int start_y = 100;
+    int arm_len = 100;
+    int bucket_1_len = 30;
+    int bucket_2_len = 20;
+    int arm_end_x = start_x + arm_len * cos(arm_angle);
+    int arm_end_y = start_y + arm_len * sin(-arm_angle);
+    int bucket_1_x = arm_end_x + bucket_1_len * cos(bucket_angle - arm_angle);
+    int bucket_1_y = arm_end_y + bucket_1_len * sin(bucket_angle - arm_angle);
+    int bucket_2_x =
+        arm_end_x + bucket_2_len * cos(bucket_angle - arm_angle - 1.2);
+    int bucket_2_y =
+        arm_end_y + bucket_2_len * sin(bucket_angle - arm_angle - 1.2);
+    cv::Point armBase(start_x, start_y);
+    cv::Point armEnd(arm_end_x, arm_end_y);
+    cv::Point bucket1(bucket_1_x, bucket_1_y);
+    cv::Point bucket2(bucket_2_x, bucket_2_y);
+    cv::Point groundStart(start_x, start_y + 2);
+    cv::Point groundEnd(start_x + arm_len + bucket_1_len, start_y + 2);
 
+    cv::line(cv_ptr->image, armBase, armEnd, cv::Scalar(0, 0, 255), 2,
+             cv::LINE_8);
+    cv::line(cv_ptr->image, armEnd, bucket1, cv::Scalar(0, 127, 255), 2,
+             cv::LINE_8);
+    cv::line(cv_ptr->image, armEnd, bucket2, cv::Scalar(0, 127, 255), 2,
+             cv::LINE_8);
+    cv::line(cv_ptr->image, groundStart, groundEnd, cv::Scalar(0, 255, 0), 2,
+             cv::LINE_8);
     image_pub_.publish(cv_ptr->toImageMsg());
   }
 
-  void textCb(const can_convert::ArmStatus::ConstPtr &msg) {
-    arm_angle = msg->arm_angle * 3.14 / 180; // Converting degrees to radians
-    bucket_angle = msg->bucket_angle * 3.14 / 180;
-    // Update overlay text
-    std::stringstream ss;
-    ss << "Bucket Angle: " << int(-bucket_angle)
-       << " deg, Arm Angle: " << int(arm_angle) << " deg";
-    overlay_text = ss.str();
-  }
-
 private:
-  ros::NodeHandle nh_;
+  ros::NodeHandle private_nh_{"~"};
   image_transport::ImageTransport it_;
   image_transport::Subscriber image_sub_;
   image_transport::Publisher image_pub_;
---
+++ catkin_ws/src/mapping/src/marker_node.cpp
@@ -0,0 +1,90 @@
+#include <geometry_msgs/Point.h>
+#include <ros/ros.h>
+#include <visualization_msgs/Marker.h>
+
+int main(int argc, char **argv)
+{
+    ros::init(argc, argv, "line_markers");
+    ros::NodeHandle nh;
+    ros::Publisher marker_pub =
+        nh.advertise<visualization_msgs::Marker>("visualization_marker", 10);
+
+    ros::Rate r(10);
+
+    // Define the 2D array of line coordinates
+    float berm_x = 5.38;
+    float berm_y = 0.6;
+    float berm_width = 2;
+    float berm_height = 0.7;
+
+    float column_size = 0.5;
+    float column_x = 3.88 - column_size / 2;
+    float column_y = 2.5;
+
+    float lines[][4] = {
+        {3.88, 0, 3.88, 5},
+        {3.88, 2, 6.88, 2},
+        {0, 0, 6.88, 0},
+        {6.88, 0, 6.88, 5},
+        {6.88, 5, 0, 5},
+        {0, 5, 0, 0},
+        {berm_x - berm_width / 2, berm_y - berm_height / 2,
+            berm_x + berm_width / 2, berm_y - berm_height / 2},
+        {berm_x + berm_width / 2, berm_y - berm_height / 2,
+            berm_x + berm_width / 2, berm_y + berm_height / 2},
+        {berm_x + berm_width / 2, berm_y + berm_height / 2,
+            berm_x - berm_width / 2, berm_y + berm_height / 2},
+        {berm_x - berm_width / 2, berm_y + berm_height / 2,
+            berm_x - berm_width / 2, berm_y - berm_height / 2},
+        {column_x - column_size / 2, column_y - column_size / 2,
+            column_x + column_size / 2, column_y - column_size / 2},
+        {column_x + column_size / 2, column_y - column_size / 2,
+            column_x + column_size / 2, column_y + column_size / 2},
+        {column_x + column_size / 2, column_y + column_size / 2,
+            column_x - column_size / 2, column_y + column_size / 2},
+        {column_x - column_size / 2, column_y + column_size / 2,
+            column_x - column_size / 2, column_y - column_size / 2},
+    };
+
+    // Number of lines
+    const int num_lines = sizeof(lines) / sizeof(lines[0]);
+
+    while (ros::ok())
+    {
+        visualization_msgs::Marker line_list;
+        line_list.header.frame_id = "map";
+        line_list.header.stamp = ros::Time::now();
+        line_list.ns = "lines";
+        line_list.action = visualization_msgs::Marker::ADD;
+        line_list.pose.orientation.w = 1.0;
+        line_list.id = 0;
+        line_list.type = visualization_msgs::Marker::LINE_LIST;
+
+        // Line width
+        line_list.scale.x = 0.02;
+
+        // Line color (red)
+        line_list.color.r = 1.0;
+        line_list.color.a = 0.5;
+
+        // Loop over the lines array and add points to the marker
+        for (int i = 0; i < num_lines; ++i)
+        {
+            geometry_msgs::Point p1, p2;
+            p1.x = lines[i][0];
+            p1.y = lines[i][1];
+            p1.z = 0;
+            p2.x = lines[i][2];
+            p2.y = lines[i][3];
+            p2.z = 0;
+            line_list.points.push_back(p1);
+            line_list.points.push_back(p2);
+        }
+
+        marker_pub.publish(line_list);
+
+        r.sleep();
+    }
+
+    return 0;
+}
