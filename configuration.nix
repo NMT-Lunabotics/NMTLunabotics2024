1cf5b426f9df2313067278b9ca10ff0c900044e0
Alex Bethel <abethel@cs.nmt.edu>
2024-05-16 18:10:30 -0400

    Add framerate ROS parameters

--- configuration.nix
+++ configuration.nix
@@ -121,6 +121,9 @@
       namespace = "arm_cam";
       remap._video_device =
         "/dev/v4l/by-id/usb-Arducam_Technology_Co.__Ltd._Arducam_USB_Camera_UC684-video-index0";
+      rosParams = pkgs.writeText "params" ''
+        framerate: 10
+      '';
     };
 
     usb-cam-logitech = {
@@ -129,6 +132,9 @@
       namespace = "funk_cam";
       remap._video_device =
         "/dev/v4l/by-id/usb-Sonix_Technology_Co.__Ltd._USB_Live_Camera_SN0001-video-index0";
+      rosParams = pkgs.writeText "params" ''
+        framerate: 10
+      '';
     };
 
     heartbeat_client = {
